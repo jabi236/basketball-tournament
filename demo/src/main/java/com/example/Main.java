1767f937d38c1294d813f960f4c409bc5a5ef040
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-02 17:17:54 -0400

    Added calls to test conference class

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,15 +2,11 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        Player p = new Player();
-        p.setId(1);
-        p.setName("Reed Sheppard");
-        p.setPPG(12.5);
-        System.out.println("Season Record for " + p.getName());
-        for(int i = 0; i < 10; i++){
-            System.out.println("Game " + (i+1) + " Points: " + p.getPoints());
-        }
-        System.out.println("PPG (Points Per Game) over " + p.getNumGames() + " games: " + p.getPPG());
+        Conference conf = new Conference();
+
+        conf.read();
+        conf.print();
+        conf.printRoster();
         
     }
 }
