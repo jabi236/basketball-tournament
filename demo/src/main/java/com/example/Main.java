72ab3e266b2cf268c7b042abd46349a992265e7f
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-03 19:03:50 -0400

    Updated to test matches on conference

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -9,6 +9,12 @@ public class Main {
         conf.read();
         conf.print();
         conf.printRoster();
-        
+        for(int i = 0; i < 8; i++){
+            for(int j = i + 1; j < 8; j++){
+                conf.match(conf.getTeam(i),conf.getTeam(j));
+                conf.match(conf.getTeam(j),conf.getTeam(i));
+            }
+        }
+        conf.print();
     }
 }
