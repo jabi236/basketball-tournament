6c3423ba375220aa2c4905bf97f520d3413b83ce
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-04 01:48:57 -0400

    Changed method to search by id instead of index

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -5,16 +5,17 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Conference conf = new Conference();
-
         conf.read();
         conf.print();
         conf.printRoster();
-        for(int i = 0; i < 8; i++){
-            for(int j = i + 1; j < 8; j++){
-                conf.match(conf.getTeam(i),conf.getTeam(j));
-                conf.match(conf.getTeam(j),conf.getTeam(i));
+        for(int i = 0; i < conf.getNumTeams(); i++){
+            for(int j = i + 1; j < conf.getNumTeams(); j++){
+                // get team by teamid. (0 + 1) * 100 
+                conf.match(conf.searchTeam((i + 1) * 100),conf.searchTeam((j + 1) * 100));
+                conf.match(conf.searchTeam((j + 1) * 100),conf.searchTeam((i + 1) * 100));
             }
         }
+        conf.rankedSort();
         conf.print();
     }
 }
