5edf0fdd6753217c9b52f5a926510c0a5caed580
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-05 18:11:01 -0400

    Set ncaa ranks for teams and added a method to print top 25 teams in the ncaa

--- demo/src/main/java/com/example/Season.java
+++ demo/src/main/java/com/example/Season.java
@@ -7,20 +7,68 @@ import java.io.InputStreamReader;
 
 public class Season extends Conference {
     static final int MAX_CONFERENES = 10;
-    
-    private Conference[] confs;
+    static final int MAX_TEAMS_MATCH = 2;
+    static final int MAX_GAMES = 16;
+    static final int TOP_25 = 25;
     private int numConfs;
+    private int numGames;
+    private Conference[] confs;
+    private Team[] ranked;
+    private int[][] schedule;
     public Season(){
         confs = new Conference[MAX_CONFERENES];
+        ranked = new Team[TOP_25];
+        schedule = new int[MAX_GAMES*MAX_CONFERENES*MAX_TEAMS][MAX_TEAMS_MATCH];
         numConfs = 0;
+        numGames = 0;
     }
     public void setNumConfs(int newNum){
         numConfs = newNum;
     }
+    public void setNumGames(int newNum){
+        numGames = newNum;
+    }
     public int getNumConfs(){return numConfs;}
-    public void schedule(){
+    public int getNumGames(){return numGames;}
+    public Conference getConference(int idx){
+        if(idx < 0 || idx > MAX_CONFERENES){
+            Conference emptyConference = new Conference();
+            return emptyConference;
+        }
+        return confs[idx];
+    }
+    public Conference searchConference(int searchId){
+        if(searchId < 0){
+            Conference emptyConference = new Conference();
+            return emptyConference;
+        }
         for(int i = 0; i < numConfs; i++){
-
+            if(confs[i].getId() == searchId){
+                return confs[i];
+            }
+        }
+        Conference emptyConference = new Conference();
+        return emptyConference;
+    }
+    public void generateScedule(){
+        for(int i=0; i < numConfs; i++){
+            for(int j=0; j < confs[i].getNumTeams(); j++){
+                for(int k=j+1; k < confs[i].getNumTeams(); k++){
+                    // set 2d array of two team ids. Home, away
+                    schedule[numGames][0] = confs[i].teams[j].getId();
+                    schedule[numGames][1] = confs[i].teams[k].getId();
+                    numGames++;
+                }  
+            }
+            // play teams again, but swithc home and away
+            for(int j=0; j < confs[i].getNumTeams(); j++){
+                for(int k=j+1; k < confs[i].getNumTeams(); k++){
+                    // set 2d array of two team ids. Home, away
+                    schedule[numGames][0] = confs[i].teams[k].getId();
+                    schedule[numGames][1] = confs[i].teams[j].getId();
+                    numGames++;
+                }  
+            }
         }
     }
     public Team game(Team t1, Team t2){
@@ -49,30 +97,55 @@ public class Season extends Conference {
         System.out.println(winner.getName() + " Wins!");
         return winner;
     }
+    public void setRanks(){
+        int totTeams = 0;
+        //get teams
+        for(int i = 0; i < numConfs; i++){
+            for(int j = 0; j < confs[i].getNumTeams(); j++){
+                ranked[totTeams] = confs[i].teams[j];
+                totTeams++;
+            }
+        }
+        //sort
+        for(int i = 0; i < totTeams; i++){
+            for(int j = 1; j < totTeams - i; j++){
+                if(ranked[j-1].getTPower() < ranked[j].getTPower()){
+                    Team temp = ranked[j-1];
+                    ranked[j-1] = ranked[j];
+                    ranked[j] = temp;
+                }
+            }
+        }
+        // set ncaa ranks
+        for(int i = 0; i < totTeams; i++){
+            ranked[i].setRank_in_ncaa(i+1);
+            for(int j = 0; j < numConfs; j++){
+                for(int k = 0; k < confs[j].getNumTeams(); k++){
+                    if(confs[j].teams[k] == ranked[i]){
+                        //copy ranks from ranked lists to conference list
+                        confs[j].teams[k].setRank_in_ncaa(ranked[i].getRank_in_ncaa());
+                    }
+                }
+            }
+        }
+    }
     public void run() throws IOException{
         for(int i = 0; i < numConfs; i++){
             read();
+            generateScedule();
+            //printSchedule();
             for(int j = 0; j < numConfs; j++){
                 confs[i].print();
-                confs[i].printRoster();
+                //confs[i].printRoster();
             }
-            int numT = confs[i].getNumTeams();
-            for(int j = 0; j < numT; j++){
-                for(int k = j + 1; k < numT; k++){
-                    // get team by teamid. (0 + 1) * 100 
-                    game(confs[i].searchTeam((j + 1) * 100),confs[i].searchTeam((k + 1) * 100));
-                }
-            }
-            confs[i].rankedSort();
-            for(int j = 0; j < numT; j++){
-                for(int k = j + 1; k < numT; k++){
-                    // get team by teamid. (0 + 1) * 100 
-                    game(confs[i].searchTeam((k + 1) * 100),confs[i].searchTeam((j + 1) * 100));
-                }
+            for(int j = 0; j < numGames; j++){
+                game(confs[i].searchTeam(schedule[j][0]), confs[i].searchTeam(schedule[j][1]));
             }
             confs[i].rankedSort();
         }
+        setRanks();
         print();
+        printTop25();
     }
     public void read() throws NumberFormatException, IOException{
         String[] conferences = {"sec.txt"};
@@ -133,6 +206,24 @@ public class Season extends Conference {
         System.out.println("==================== SEASON ====================");
         for(int i = 0; i < numConfs; i++){
             confs[i].printRoster();
+        }
+    }
+    public void printSchedule(){
+        System.out.println("=================== SCHEDULE ===================");
+        for(int i = 0; i < numConfs; i++){
+            System.out.println("========== " + confs[i].getName() + " ==========");
+            for(int j = 0; j < numGames; j++){
+                Team t1 = confs[i].searchTeam(schedule[j][0]);
+                Team t2 = confs[i].searchTeam(schedule[j][1]);
+                System.out.println(t1.getName() + " | " + t2.getName());
+            }
+        }
+    }
+    public void printTop25(){
+        //TODO: change 8 to TOP_25 when enough teams
+        System.out.println("==================== TOP 25 ===================");
+        for(int i = 0; i < 8; i++){
+            ranked[i].print();
         }
     }
 }
