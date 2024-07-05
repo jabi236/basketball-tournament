d6196d17aa167cd22a79c1f2ed9b979b19f19e85
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-05 00:40:05 -0400

    Added test calls for season

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -4,18 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Conference conf = new Conference();
-        conf.read();
-        conf.print();
-        conf.printRoster();
-        for(int i = 0; i < conf.getNumTeams(); i++){
-            for(int j = i + 1; j < conf.getNumTeams(); j++){
-                // get team by teamid. (0 + 1) * 100 
-                conf.match(conf.searchTeam((i + 1) * 100),conf.searchTeam((j + 1) * 100));
-                conf.match(conf.searchTeam((j + 1) * 100),conf.searchTeam((i + 1) * 100));
-            }
-        }
-        conf.rankedSort();
-        conf.print();
+        Season s = new Season();
+        int numConfs = 1;
+        s.setNumConfs(numConfs);
+        s.run();
     }
 }
