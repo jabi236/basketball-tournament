5c2054a1bd3f0378810975f78e60b4d36e034e3e
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-05 18:05:31 -0400

    Added tpower and ranks for conference and ncaa

--- demo/src/main/java/com/example/Team.java
+++ demo/src/main/java/com/example/Team.java
@@ -8,12 +8,18 @@ public class Team extends Player{
     protected Player[] players;
     private int wins;
     private int losses;
+    private double tpower; //team power
+    private int rank_in_conf;
+    private int rank_in_ncaa;
     public Team(){
         name = "";
         id = 0;
         numPlayers = 0;
         wins = 0;
         losses = 0;
+        tpower = 0;
+        rank_in_conf = 0;
+        rank_in_ncaa = 0;
         players = new Player[MAX_PLAYERS];
     }
     public void setName(String newName){
@@ -25,17 +31,36 @@ public class Team extends Player{
     public void setNumPlayers(int newNum){
         numPlayers = newNum;
     }
+    public void setTPower(double newPower){
+        tpower = newPower;
+    }
     public void addWins(){
         wins = wins + 1;
     }
     public void addLosses(){
         losses = losses +1;
     }
+    public void setRank_in_conf(int newRank){
+        rank_in_conf = newRank;
+    }
+    public void setRank_in_ncaa(int newRank){
+        rank_in_ncaa = newRank;
+    }
     public String getName(){return name;}
     public int getId(){return id;}
     public int getWins(){return wins;}
     public int getLosses(){return losses;}
     public int getNumPlayers(){return numPlayers;}
+    public double getTPower(){return tpower;}
+    public int getRank_in_conf(){return rank_in_conf;}
+    public int getRank_in_ncaa(){return rank_in_ncaa;}
+    public double getTotPPG(){
+        double totPPG = 0;
+        for(int i = 0; i < numPlayers; i++){
+            totPPG = totPPG + players[i].getPPG();
+        }
+        return totPPG;
+    }
     public Player getPlayer(int idx){
         if(idx < 0 || idx > MAX_PLAYERS){
             Player emptyPlayer = new Player();
@@ -60,6 +85,10 @@ public class Team extends Player{
         }
     }
     public void print(){
-        System.out.println(name.toUpperCase() + " " + wins + "-" + losses);
+        String rank = "";
+        if(rank_in_ncaa != 0 && rank_in_ncaa <= 25){
+            rank = "(" + rank_in_ncaa + ") ";
+        }
+        System.out.println(rank + name.toUpperCase() + " " + wins + "-" + losses);
     }
 }
