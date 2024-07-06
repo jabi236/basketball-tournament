0cb95423609294d144c786c7dcabfdabaad182c9
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-06 01:14:53 -0400

    Changed main to just call go method in userInt class

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,14 +2,9 @@ package com.example;
 
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Season s = new Season();
-        int numConfs = 1;
-        s.setNumConfs(numConfs);
-        s.run();
-        Tournament t = new Tournament();
-        t.getEntries(s);
-        t.printEntries();
+public class Main extends UserInt{
+    public static void main(String[] args) throws IOException{
+        UserInt x = new UserInt();
+        x.go();
     }
 }
