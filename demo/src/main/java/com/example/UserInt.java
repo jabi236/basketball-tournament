591e45481ddf1fee746bd258a53f0024bb3fc7ac
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-09 17:32:11 -0400

    Set menu and made switch case to envoke season and tournament simulations

--- demo/src/main/java/com/example/UserInt.java
+++ demo/src/main/java/com/example/UserInt.java
@@ -5,15 +5,26 @@ import java.io.IOException;
 public class UserInt extends Menu{
     private Menu mainMenu;
 
-    public UserInt(){
-
+    public UserInt() throws IOException{
+        mainMenu = new Menu();
+        // set main menu
+        mainMenu.setTitle("NCAA BASKETBALL SIMULATOR");
+        mainMenu.setErrorMsg("Invalid option! Try again!");
+        mainMenu.addOption('S', "Simulate Regular Season");
+        mainMenu.addOption('T', "Simulate Tournament");
+        mainMenu.addOption('X', "Exit");
     }
-    public void go() throws IOException{
-        Season s = new Season();
-        int numConfs = 1;
-        s.setNumConfs(numConfs);
-        s.playSeason();
-        Tournament t = new Tournament();
-        t.playTournamnet(8, s);
+    
+    public void go() throws IOException, InterruptedException{
+        char opt = ' ';
+        while(opt != 'X'){
+            opt = mainMenu.doMenu();
+            switch(opt){
+                case 'S': Season s = new Season(); s.playSeason(); break;
+                case 'T': Season s2 = new Season(); s2.playSeason(); 
+                          Tournament t = new Tournament(); t.playTournamnet(8, s2); break;
+                case 'X': System.out.println("ENDING SIMULATION");
+            }
+        }
     }
 }
