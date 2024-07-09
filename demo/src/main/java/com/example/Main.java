cd7e7c97dc635f156668c6519e4993a4e681c1ff
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-09 17:33:02 -0400

    Addeded exception throw

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -2,8 +2,8 @@ package com.example;
 
 import java.io.IOException;
 
-public class Main extends UserInt{
-    public static void main(String[] args) throws IOException{
+public class Main{
+    public static void main(String[] args) throws IOException, InterruptedException{
         UserInt x = new UserInt();
         x.go();
     }
