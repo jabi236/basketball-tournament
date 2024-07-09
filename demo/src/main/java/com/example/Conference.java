ee5d6f18359855d53a19e1276f4fa23d06dfbf13
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-09 17:26:07 -0400

    Added comments for method descriptions

--- demo/src/main/java/com/example/Conference.java
+++ demo/src/main/java/com/example/Conference.java
@@ -7,6 +7,11 @@ public class Conference extends Team{
     private int numGames;
     protected Team[] teams;
     private int confRank;
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: constructor
+    // Date: 7/3/24
+    // Description: initializes the objects (data members) used by the class
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public Conference(){
         numTeams = 0;
         confRank = 0;
@@ -14,6 +19,11 @@ public class Conference extends Team{
         name = "";
         teams = new Team[MAX_TEAMS];
     }
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Functions: sets
+    // Date: 7/3/24
+    // Description: set attributes with new values
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public void setName(String newName){
         name = newName;
     }
@@ -26,18 +36,33 @@ public class Conference extends Team{
     public void setNumGames(int newNum){
         numGames = newNum;
     }
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Functions: gets
+    // Date: 7/3/24
+    // Description: return attributes value
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public String getName(){return name;}
     public int getNumTeams(){return numTeams;}
     public int getNumGames(){return numGames;}
     public int getConfRank(){return confRank;}
-    public Team getTeam(int idx){
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: searchTeam by index
+    // Date: 7/3/24
+    // Description: searches for team by index of partial array of team objects, if index is out of range it returns an empty team
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    public Team searchTeam_byIndex(int idx){
         if(idx < 0 || idx > MAX_TEAMS){
             Team emptyTeam = new Team();
             return emptyTeam;
         }
         return teams[idx];
     }
-    public Team searchTeam(int searchId){
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: searchTeam by id
+    // Date: 7/3/24
+    // Description: searches for team by id of team objects, if it is not found or is not in range, return an empty team
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    public Team searchTeam_byId(int searchId){
         if(searchId < 0){
             Team emptyTeam = new Team();
             return emptyTeam;
@@ -50,7 +75,12 @@ public class Conference extends Team{
         Team emptyTeam = new Team();
         return emptyTeam;
     }
-    public void rankedSort(){
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: sortTeams
+    // Date: 7/3/24
+    // Description: use bubble sort to sort team objects from highest record to lowest record
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    public void sortTeams(){
         for(int i = 0; i < numTeams; i++){
             for(int j = 1; j < numTeams - i; j++){
                 if(teams[j-1].getRecord() < teams[j].getRecord()){
@@ -62,22 +92,30 @@ public class Conference extends Team{
         }
         for(int i = 0; i < numTeams; i++){
             teams[i].setRank_in_conf(i+1);
-            teams[i].setTPower((1 + (0.1 * (10 - confRank))*(teams[i].getRecord() + (numTeams - teams[i].getRank_in_conf()) + teams[i].getTotPPG())));
+            // generate power number for each team based on the rank of their conference, their season record, their rank within the conference, and the team's total PPG
+            teams[i].setTPower((1 + (0.05 * (10 - confRank)) * ((teams[i].getRecord() * 2) + (numTeams - teams[i].getRank_in_conf()) + teams[i].getTotPPG())));
         }
     }
-
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: print roster
+    // Date: 7/3/24
+    // Description: print conference name, team name, then print players in team by evoking player's object print method
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public void printRoster(){
         System.out.println("============= " + name.toUpperCase() + " CONFERENCE =============");
         for(int i = 0; i < numTeams; i++){
             teams[i].printRoster();
         }
     }
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: print
+    // Date: 7/3/24
+    // Description: print conference name, then print teams by invoking team objects print method
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public void print(){
         System.out.println("============= " + name.toUpperCase() + " CONFERENCE =============");
         for(int i = 0; i < numTeams; i++){
             teams[i].print();
         }
     }
-
-
 }
