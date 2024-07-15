bd69ead24d6daa6849d422b482af1740c3520267
jabi236 <123780845+jabi236@users.noreply.github.com>
2024-07-15 18:15:23 -0400

    Updated call to playtournamnet with updated number of parameters

--- demo/src/main/java/com/example/UserInt.java
+++ demo/src/main/java/com/example/UserInt.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 public class UserInt extends Menu{
     private Menu mainMenu;
-
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Function: constructor
+    // Date: 7/5/24
+    // Description: initializes the objects (data members) used by the class
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public UserInt() throws IOException{
         mainMenu = new Menu();
         // set main menu
@@ -14,7 +18,11 @@ public class UserInt extends Menu{
         mainMenu.addOption('T', "Simulate Tournament");
         mainMenu.addOption('X', "Exit");
     }
-    
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
+    // Functions: go
+    // Date: 7/5/24
+    // Description: This is the main function of the simulation
+    //-------------------------------------------------------------------------------------------------------------------------------------------------
     public void go() throws IOException, InterruptedException{
         char opt = ' ';
         while(opt != 'X'){
@@ -22,7 +30,7 @@ public class UserInt extends Menu{
             switch(opt){
                 case 'S': Season s = new Season(); s.playSeason(); break;
                 case 'T': Season s2 = new Season(); s2.playSeason(); 
-                          Tournament t = new Tournament(); t.playTournamnet(8, s2); break;
+                          Tournament t = new Tournament(); t.playTournamnet(s2); break;
                 case 'X': System.out.println("ENDING SIMULATION");
             }
         }
